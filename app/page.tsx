52abe05a72362b2bed3f9e9df8b50142d6acb00a
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-11-23 22:28:18 +0700

    Refactor layout and page components: add Search component to RootLayout and remove from Home

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import {
 import { fetchJishoData } from "@/services";
 import { JishoResponse } from "@/interfaces";
 import { useQuery } from "@tanstack/react-query";
-import { Search } from "./_components";
 import { useState } from "react";
 
 
@@ -26,7 +25,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Search />
+        
       </main>
     </div>
   );
