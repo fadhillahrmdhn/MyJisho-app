6b317aa201b7d4a25cc2041a26fe89dc98188d75
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-11-24 22:42:36 +0700

    Add detail page component with data fetching and rendering for Jisho entries

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,10 @@ import { JishoResponse } from "@/interfaces";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { useSearchStore } from "@/store";
+import Link from "next/link";
 
 export default function Home() {
-  const queryClient = new QueryClient();
+
   const { search } = useSearchStore();
   const { data, isLoading, error } = useQuery<JishoResponse>({
     queryKey: ['jishoData', search],
@@ -21,12 +22,17 @@ export default function Home() {
     refetchOnWindowFocus: false
   });
 
-  console.log(data);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
+        {data?.data &&
+          data.data.map((item, index) => (
+            // Gunakan properti unik sebagai key, seperti slug atau index
+            <div key={item.slug || index}>
+              <Link href={`/detail/${item.slug}/${index}`}>{item.slug}</Link>
+            </div>
+          ))}
       </main>
     </div>
   );
